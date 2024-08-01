6b05e7dd511c70c991410ea0b68ce48f33944be9
Piyush Sharma <piyushsharma29052004@gmail.com>
2024-08-01 17:22:11 +0530

    react1.0

---
+++ basic-react-app/src/Title.jsx
@@ -0,0 +1,15 @@
+function Title(){
+
+
+    let name="rama";
+    return(
+    <div>
+        <h1>I am the Title</h1>
+        <p>2*2 ={2*2}</p>
+        <p>{name.toUpperCase()}</p>
+        </div>);
+  }
+  
+
+//   export default Title;
+export {Title};
